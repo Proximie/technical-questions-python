21bce0db40531ffa3090e25f04e058180621e453
Rob Pethick <rpethick@proximie.com>
2026-01-13 11:21:40 +0000

    update list of incorrect values in flatten list

--- flatten_list.py
+++ flatten_list.py
@@ -1,7 +1,21 @@
-# Get a flat list without empty lists empty dicts and None values
-
+# Get a flat list without None, 0, empty lists, empty dicts, empty sets, empty tuples, empty ranges and empty string
 # For example: input: [2, [3, 5, [1], [[32]], 3], None] output: [2, 3, 5, 1, 32, 3]
 
 def execute(input_list):
-    # Your code goes here!
-    raise Exception("Not implemented yet")
+    
+    flat_list = []
+    for i in input_list:
+        if i== None:
+            continue
+        elif i == 0:
+            continue
+        elif i == '':
+            continue
+        elif i =={}:
+            continue
+        elif type(i) == list:
+            print('recursion')
+            flat_list += execute(i)
+        else:
+            flat_list.append(i)
+    return flat_list
