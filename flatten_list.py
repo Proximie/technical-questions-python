2b0472b747d3579cdd8cd3ef4cf57d03f234231c
Rob Pethick <rpethick@proximie.com>
2026-01-13 13:45:52 +0000

    fixup

--- flatten_list.py
+++ flatten_list.py
@@ -2,20 +2,5 @@
 # For example: input: [2, [3, 5, [1], [[32]], 3], None] output: [2, 3, 5, 1, 32, 3]
 
 def execute(input_list):
-    
-    flat_list = []
-    for i in input_list:
-        if i== None:
-            continue
-        elif i == 0:
-            continue
-        elif i == '':
-            continue
-        elif i =={}:
-            continue
-        elif type(i) == list:
-            print('recursion')
-            flat_list += execute(i)
-        else:
-            flat_list.append(i)
-    return flat_list
+    # Your code goes here!
+    raise Exception("Not implemented yet")
